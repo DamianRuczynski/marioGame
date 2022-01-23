3d61fe9c94a1a49a3c7da9f4f1532a5245b5a59a
Damian Ruczynski <damian.m.ruczynski@gmail.com>
2022-01-23 21:46:59 +0100

    mario can collect money and grow up after mushroom

--- marioKaboom.js
+++ marioKaboom.js
@@ -29,7 +29,7 @@ scene("game", () => {
         '                                                                   ',
         '                                                                   ',
         '                                                                   ',
-        '          %  =*=     %                                             ',
+        '            %  =*=     %                                           ',
         '                                                                   ',
         '                                           -+                      ',
         '                              ^     ^      ()                      ',
@@ -40,11 +40,11 @@ scene("game", () => {
         height: 20,
         '=': [sprite('block'), solid()],
 
-        '$': [sprite('coin')],
+        '$': [sprite('coin'), 'coin'],
         '}': [sprite('unboxed'), solid()],
 
-        '^': [sprite('evil-shroom'), solid()],
-        '#': [sprite('mushroom'), solid()],
+        '^': [sprite('evil-shroom'), solid(), 'dangerous'],
+        '#': [sprite('mushroom'), solid(), 'mushroom', body()],
         '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
         '%': [sprite('surprise'), solid(), 'coin-surprise'],
 
@@ -57,11 +57,11 @@ scene("game", () => {
     const gameLevel = addLevel(map, levelCfg)
 
     const scoreLabel = add([
-        text('tesstScore'),
+        text("Score: " + 0),
         pos(30, 6),
         layer('ui'),
         {
-            value: 'tesstsuka',
+            value: 0,
         }
     ])
 
@@ -73,7 +73,8 @@ scene("game", () => {
         return {
             update() {
                 if (isBig) {
-                    time -= dt()
+                    CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+                    timer -= dt()
                     if (timer <= 0) {
                         this.smallify()
                     }
@@ -83,11 +84,13 @@ scene("game", () => {
                 return isBig
             },
             smallify() {
+                CURRENT_JUMP_FORCE = JUMP_FORCE
                 this.scale = vec2(1)
                 timer = 0
                 isBig = false
             },
             biggify(time) {
+                CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
                 this.scale = vec2(2)
                 timer = time
                 isBig = true
@@ -103,8 +106,44 @@ scene("game", () => {
         origin('bot'),
     ])
 
+    action('mushroom', (m) => {
+        m.move(20, 0)
+    })
+
+    player.on("headbump", (obj) => {
+        if (obj.is('coin-surprise')) {
+            gameLevel.spawn('$', obj.gridPos.sub(0, 1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0, 0))
+        }
+
+        if (obj.is('mushroom-surprise')) {
+            gameLevel.spawn('#', obj.gridPos.sub(0, 1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0, 0))
+        }
+    })
+
+    player.collides('mushroom', (m) => {
+        destroy(m)
+        player.biggify(4)
+    })
+
+
+    player.collides('coin', (c) => {
+        destroy(c)
+        scoreLabel.value++
+            scoreLabel.text = "Score: " + scoreLabel.value
+    })
+
+    player.collides('dangerous', (d) => {
+        go('lose', { score: scoreLabel.value })
+    })
+
     const MOVE_SPEED = 120
     const JUMP_FORCE = 360
+    const BIG_JUMP_FORCE = 540
+    let CURRENT_JUMP_FORCE = JUMP_FORCE
 
     keyDown('left', () => {
         player.move(-MOVE_SPEED, 0)
@@ -116,8 +155,11 @@ scene("game", () => {
 
     keyPress('space', () => {
         if (player.grounded()) {
-            player.jump(JUMP_FORCE)
+            player.jump(CURRENT_JUMP_FORCE)
         }
+    })
+    scene('lose', ({ score }) => {
+        add([text(score, 32), origin('center'), pos(width() / 2, height(), 2)])
     })
 })
 
